13532bba37ad3cab931216ff32d8ae2035a94ef1
jacky <arthur.bojic@gmail.com>
2024-09-18 00:30:05 +0300

    add context_processor for cart

--- cart/urls.py
+++ cart/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from views import cart_add, cart_detail, cart_remove
+from .views import cart_add, cart_detail, cart_remove
 
 app_name = 'cart'
 
--- shop/views.py
+++ shop/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Category, Product
+from cart.forms import CartAddProductForm
 
 def product_list(request, category_slug=None):
     category = None
     categories = Category.objects.all()
-    products = Product.objects.first(active=True)
+    products = Product.objects.filter(available=True).first()
     if category_slug:
         category = get_object_or_404(Category, slug=category_slug)
         products = products.filter(category=category)
@@ -15,7 +16,9 @@ def product_list(request, category_slug=None):
                    'categories': categories})
 
 def product_detail(request, id, slug):
-    product = get_object_or_404(Product, id=id, slug=slug, active=True)
+    product = get_object_or_404(Product, id=id, slug=slug, available=True)
+    cart_product_form = CartAddProductForm()
     return render(request,
                   'shop/product/detail.html',
-                  {'product': product})
+                  {'product': product,
+                   'cart_product_form': cart_product_form})
